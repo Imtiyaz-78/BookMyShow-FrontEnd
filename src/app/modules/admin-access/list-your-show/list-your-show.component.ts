b0dac8424cbc49f1505e4573e346049e585c3e5e
Md Imtiyaz <md.imtiyaz@in2ittech.com>
2025-09-03 18:47:27 +0530

    UI of show all list

--- src/app/modules/admin-access/list-your-show/list-your-show.component.ts
+++ src/app/modules/admin-access/list-your-show/list-your-show.component.ts
@@ -1,6 +1,6 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-list-your-show',
@@ -9,11 +9,31 @@ import { BsModalService } from 'ngx-bootstrap/modal';
   styleUrl: './list-your-show.component.scss',
 })
 export class ListYourShowComponent {
+  modalRef?: BsModalRef;
   constructor(private modalService: BsModalService) {}
 
+  /**
+   * @description Opens the service modal with the given template reference.
+   * @author Imtiyaz
+   * @param {TemplateRef<any>} openServiceModal - The modal template to be displayed.
+   * @returns {void}
+   */
   onOpenServiceModal(openServiceModal: TemplateRef<any>) {
-    this.modalService.show(openServiceModal, {
+    this.modalRef = this.modalService.show(openServiceModal, {
       class: 'modal-dialog-centered',
+      ignoreBackdropClick: true,
+      keyboard: false,
     });
+  }
+
+  /**
+   * @description Closes the currently opened modal if it exists.
+   * @author Imtiyaz
+   * @returns {void}
+   */
+  onCloseModal() {
+    if (this.modalRef) {
+      this.modalRef.hide();
+    }
   }
 }
